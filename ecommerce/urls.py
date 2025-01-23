d17bdbcc711e461d503b47f71399821144092ca3
LTIsaimaniroop <saimaniroop.10687590@ltimindtree.com>
2025-01-23 20:01:37 +0530

    added cart, store, checkout functionality

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('',main, name = "main"),
     path('store/',store, name = "store"),
     path('cart/',cart, name = "cart"),
-    path('checkout/',checkout, name = "checkout")
+    path('checkout/',checkout, name = "checkout"),
+    path('update_item/',updateitem, name = "update_item")
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from testingapp.templates import *
 from .models import *
+from django.http import JsonResponse
+import json
 # Create your views here.
 
 def main(request):
@@ -9,7 +11,16 @@ def main(request):
 
 def store(request):
     all_products = Product.objects.all()
-    context = {"products": all_products}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer = customer,complete = False)
+        items = order.orderitem_set.all()
+        cartitems = order.get_cart_items
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0,'shipping':False}
+        cartitems = order['get_cart_items']
+    context = {"products": all_products, 'cartitems':cartitems}
     return render(request, 'store.html', context)
 
 def cart(request):
@@ -17,11 +28,12 @@ def cart(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer = customer,complete = False)
         items = order.orderitem_set.all()
+        cartitems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
-
-    context = {'items':items, 'order':order}
+        order = {'get_cart_total':0, 'get_cart_items':0,'shipping':False}
+        cartitems = order['get_cart_items']
+    context = {'items':items, 'order':order, 'cartitems':cartitems}
     return render(request, 'cart.html', context)
 
 def checkout(request):
@@ -29,9 +41,38 @@ def checkout(request):
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer = customer,complete = False)
         items = order.orderitem_set.all()
+        cartitems = order.get_cart_items
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+        order = {'get_cart_total':0, 'get_cart_items':0,'shipping':False}
+        cartitems = order['get_cart_items']
 
-    context = {'items':items, 'order':order}
+    context = {'items':items, 'order':order, 'cartitems':cartitems}
     return render(request, 'checkout.html', context)
+
+
+def updateitem(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    print('Action:', action)
+    print('Product:', productId)
+
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+
+    orderItem, created = Orderitem.objects.get_or_create(order=order, product=product)
+    
+    if action == 'add':
+        orderItem.quantity = (orderItem.quantity + 1)
+    elif action == "remove":
+        orderItem.quantity = (orderItem.quantity - 1)
+
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
+    return JsonResponse("Item added", safe=False)
+    
